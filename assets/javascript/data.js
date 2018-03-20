eecc511b9c053da69343258ac0f08372137fdb11
BreBusby <brebusby13@gmail.com>
2018-03-20 01:01:33 -0500

    second commit

---
+++ assets/javascript/data.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+var config = {
+    apiKey: "AIzaSyAE5dWmau3ZxWPdTb49sGvDSqR2VCSOM68",
+    authDomain: "train-scheduler-4106f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4106f.firebaseio.com",
+    projectId: "train-scheduler-4106f",
+    storageBucket: "train-scheduler-4106f.appspot.com",
+    messagingSenderId: "376387335459"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+  $("#submit").on("click", function() {
+      
+      
+  }); 
+});
